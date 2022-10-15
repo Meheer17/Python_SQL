97a785564fa537b75d10a2d5cde5af11da436774
Meheer17 <meherr17.j@gmail.com>
2022-10-16 00:00:35 +0530

    Functions

---
+++ function.py
@@ -0,0 +1,194 @@
+# This file has all the various functions of SQL and also has a login feature, to ensure that an authenticated user can access the database. 
+
+# SQL_FUNCTIONS
+import mysql.connector
+
+headers = [0,'COMMON']
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="",
+    password="",
+    database=headers[1]
+)
+mc = mydb.cursor() 
+
+
+def Login():
+    name = input("Enter Username: ")
+    pas = input("Enter the Password: ")
+    mc.execute(f"USE {name};")
+    mc.execute(f"SELECT * FROM password;")
+    data = mc.fetchall()
+    global headers
+    for i in data:
+        if i[0] == pas:
+            headers = [1, name]
+        else:
+            mc.execute(f"USE COMMON;") 
+
+def DisplayDatabase():
+    mc.execute("SHOW DATABASES;")
+    data = mc.fetchall()
+    for i in range(len(data)):
+        print(f'{i + 1} - {data[i][0]}')
+    print()
+    
+    return data
+
+def SignUp():
+    database_name = input("Enter the Database Name to be Created: ")
+    pas = input("Enter Your Password: ")
+    mc.execute(f"CREATE DATABASE IF NOT EXISTS {database_name};")
+    mc.execute(f"USE {database_name};")
+    mc.execute(f"CREATE TABLE IF NOT EXISTS password(password varchar(100));")
+    mc.execute(f"INSERT INTO password VALUES ('{pas}');")
+    mydb.commit()
+    print()
+    Login()
+
+def DropDatabase():
+    data = DisplayDatabase()
+    dd = int(input("Enter the Database to be Deleted (Number): "))
+    mc.execute(f"DROP DATABASE {data[dd-1][0]}")
+
+def Auth():
+    if headers[0] == 1:
+        return True
+    else :
+        return False
+
+def ShowTable():
+    if Auth():
+        mc.execute("SHOW TABLES;")
+        data = mc.fetchall()
+        for i in range(len(data)):
+            print(f'{i + 1} - {data[i][0]}')
+        return data
+
+def CreateTable():
+    if Auth():
+        table_name = input("Enter the New Table's Name: ")
+        nof = int(input("Enter the number of Columns: "))
+        DataTypes = ['CHAR', 'VARCHAR', 'TEXT', 'INT', 'TINYINT', 'FLOAT', 'DATE', 'TIMESTAMP', 'SERIAL']
+
+        c = []
+        for i in range(nof):
+            naof = input("\nEnter the name of the column: ")
+            print()
+            for j in range(len(DataTypes)):
+                print(f'{j + 1} - {DataTypes[j]}')
+            dt = tuple(input("Enter the DataType Number and value to be parsed with space: ").split(' '))
+            if ((DataTypes[int(dt[0])-1] != "DATE") & (DataTypes[int(dt[0])-1] != "TIMESTAMP") & (DataTypes[int(dt[0])-1] != "SERIAL")):
+                dt = (DataTypes[int(dt[0])-1], int(dt[1]))
+                dt = f"{dt[0]}({dt[1]})"
+            else:
+                dt = f"{DataTypes[int(dt[0])-1]}"
+            
+            cons = Constraint()
+            txt = f"{naof} {dt} {cons}"
+            c.append(txt)
+            tet = ",".join(c)
+        mc.execute(f"CREATE TABLE {table_name}({tet});")
+
+def DesTable():
+    if Auth():
+        data = ShowTable()
+        T_N = int(input("Enter the Table Number to Show Details: "))
+        mc.execute(f"DESC {data[T_N - 1][0]};")
+        det = mc.fetchall()
+        print("| Field | Type | Null | Key | Default | Extra |")
+        for i in det:
+            print(i)
+        return (det, data, T_N)
+
+def Rename():
+    if Auth():
+        data = ShowTable()
+        print("\nWhich Column Name to Change:-")
+        for i in range(len(data)):
+            print(f'{i + 1} - {data[i][0]}')
+        rn = int(input("Enter the Table Num you want to change: "))
+        rnn = input("Enter the name you want to rename to: ")
+        if data[rn-1][0] != 'password':
+            mc.execute(f"ALTER TABLE {data[rn -1][0]} RENAME TO {rnn}")
+
+def DropTable():
+    if Auth():
+        data = ShowTable()
+        print("\nWhich Column Name to Change:-")
+        for i in range(len(data)):
+            print(f'{i + 1} - {data[i][0]}')
+        dt = int(input("Enter the table to delete: "))
+        if data[dt-1][0] != 'password':
+            mc.execute(f"DROP TABLE {data[dt-1][0]}")
+        
+def ADMTable(): # ADD DELETE MODIFY TABLE
+    if Auth():
+        types = ["ADD","DROP","MODIFY"]
+        print()
+        data = DesTable()
+        print()
+        for i in range(len(types)):
+            print(f"{i+1} - {types[i]} Column")
+        ch = int(input("Enter the Task: "))
+        if((ch == 1) | (ch == 2) | (ch == 3)):
+            if ch ==1:
+                DataTypes = ['CHAR', 'VARCHAR', 'TEXT', 'INT', 'TINYINT', 'FLOAT', 'DATE', 'TIMESTAMP', 'SERIAL']
+                naof = input("\nEnter the name of the column: ")
+                print()
+                for j in range(len(DataTypes)):
+                    print(f'{j + 1} - {DataTypes[j]}')
+                dt = tuple(input("Enter the DataType Number and value to be parsed with space: ").split(' '))
+                if ((DataTypes[int(dt[0])-1] != "DATE") & (DataTypes[int(dt[0])-1] != "TIMESTAMP") & (DataTypes[int(dt[0])-1] != "SERIAL")):
+                    dt = (DataTypes[int(dt[0])-1], int(dt[1]))
+                    dt = f"{dt[0]}({dt[1]})"
+                else:
+                    dt = f"{DataTypes[int(dt[0])-1]}"
+
+                txt = f"{naof} {dt}"
+                mc.execute(f'ALTER TABLE {data[1][data[2] -1][0]} ADD {txt};')
+            if ch == 2:
+                for i in range(len(data[0])):
+                    print(f"{i + 1} - {data[0][i][0]}")
+                drope = int(input("Enter the column to DELETE: "))
+                mc.execute(f'ALTER TABLE {data[1][data[2] -1][0]} DROP { data[0][drope - 1][0]};')
+            if ch == 3:
+                DataTypes = ['CHAR', 'VARCHAR', 'TEXT', 'INT', 'TINYINT', 'FLOAT', 'DATE', 'TIMESTAMP', 'SERIAL']
+                for i in range(len(data[0])):
+                    print(f"{i + 1} - {data[0][i][0]}")
+                drope = int(input("Enter the column to MODIFY: "))
+                for j in range(len(DataTypes)):
+                    print(f'{j + 1} - {DataTypes[j]}')
+                dt = tuple(input("Enter the DataType Number and value to be parsed with space: ").split(' '))
+                if ((DataTypes[int(dt[0])-1] != "DATE") & (DataTypes[int(dt[0])-1] != "TIMESTAMP") & (DataTypes[int(dt[0])-1] != "SERIAL")):
+                    dt = (DataTypes[int(dt[0])-1], int(dt[1]))
+                    dt = f"{dt[0]}({dt[1]})"
+                else:
+                    dt = f"{DataTypes[int(dt[0])-1]}"
+
+                txt = f"{dt}"
+                
+                cons = input("Do you want to add constraints? (Y/N): ")
+                if cons.upper() == "Y":
+                    text = Constraint()
+                    mc.execute(f'ALTER TABLE {data[1][data[2] -1][0]} MODIFY { data[0][drope - 1][0]} {txt} {text};')
+                else: 
+                    mc.execute(f'ALTER TABLE {data[1][data[2] -1][0]} MODIFY { data[0][drope - 1][0]} {txt};')
+        
+def Constraint():
+    constraints = ["NOT NULL", "DEFAULT", "UNIQUE", "PRIMARY KEY", "CHECK","FOREIGN KEY"]
+    for i in range(len(constraints)):
+        print(f'{i + 1} - {constraints[i]}')
+    c = int(input("Enter the constraint: "))
+    if((c == 5) | (c == 6)):
+        if c == 5:
+            con = input("Enter Your Condition: ")
+            text = f"CHECK({con})"
+        if c == 6:
+            tn = input("Enter the table name: ")
+            cn = input("Enter the column name to be refered: ")
+            text = f"FOREIGN KEY REFERENCES {tn}({cn})"
+    else:
+        text = f"{constraints[c - 1]}"
+    return text
