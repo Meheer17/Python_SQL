41b8f34fc8507e21fe4b5f1122022bd05c87947d
Meheer17 <meherr17.j@gmail.com>
2022-11-27 17:05:01 +0530

    Changes3

--- routes.py
+++ routes.py
@@ -1,7 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request
-# import function as sql
+import function as sql
 
 app = Flask(__name__)
+
+if sql.headers[0] == 1:
+    auth = True
+else:
+    auth = False
 
 @app.route("/", methods=["GET"])
 def home():
