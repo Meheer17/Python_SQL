255ceb1027f1ed6f6a7601ddb88c7f34255effca
Meheer17 <meherr17.j@gmail.com>
2022-11-27 16:53:09 +0530

    Changes2

--- routes.py
+++ routes.py
@@ -1,11 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request
-import function as sql
+# import function as sql
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("index.html")
+    return render_template("index.html", page='login')
 
 if __name__ == "__main__":
 	app.run()
