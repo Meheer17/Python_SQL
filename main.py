a10d46e59949e970fdb765fbdff229b33f108fb2
Meheer17 <meherr17.j@gmail.com>
2023-01-14 23:48:47 +0530

    deleted unused files

--- main.py
+++
@@ -1,4 +0,0 @@
-import function as sql # The function file has been imported as sql and the function call much be like sql.Login() Remember all the functions are capitalised. 
-# Update CustomQuery DisplayGroupBy DisplayOrderBy DisplayWithWhere DisplayAll ADMTable DropTable Rename DesTable CreateTable ShowTable Auth SignUp DisplayDatabase Login
-
-sql.Login()
