7a15edf9a0601ebbe5ebaa952ddb79a06f87cb80
Meheer17 <meherr17.j@gmail.com>
2023-01-15 20:52:34 +0530

    auth routes removed

--- routes.py
+++ routes.py
@@ -12,40 +12,39 @@ def reroute():
 def home():
     return render_template("index.html", auth = sql.Auth())
 
-if sql.Auth() == False:
-    @app.route("/login", methods=["GET","POST"])
-    def login():
-            if request.method == 'GET':
-                return render_template("login.html", page='LOGIN', auth = sql.Auth())
-            elif request.method == "POST":
-                sql.Login(request.form['email'], request.form['password'])
-                return redirect('/')
-            else:
-                return redirect('/')
 
-    @app.route("/signup", methods=["GET","POST"])
-    def signup():
-            if request.method == 'GET':
-                return render_template("login.html", page='SIGNUP', auth = sql.Auth())
-            elif request.method == "POST":
-                sql.SignUp(request.form['email'], request.form['password'])
-                return redirect('/')
-            else:
-                return redirect('/')
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html", page='LOGIN', auth = sql.Auth())
+    elif request.method == "POST":
+        sql.Login(request.form['email'], request.form['password'])
+        return redirect('/')
+    else:
+        return redirect('/')
 
-elif sql.Auth() == True:
-    @app.route("/database", methods=["GET","POST"])
-    def Display_Tables():
-            data = sql.ShowTable()
-            if desc:
-                args = request.args.to_dict().get("Table_number")
-                desc = sql.DesTable(args)
-                return render_template('database.html', tables=data, desc=desc)
-            else: 
-                return render_template('database.html', tables=data)
-else:
-    def ret():
-        return redirect(url_for("home"))
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method == 'GET':
+        return render_template("login.html", page='SIGNUP', auth = sql.Auth())
+    elif request.method == "POST":
+        sql.SignUp(request.form['email'], request.form['password'])
+        return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route("/database", methods=["GET","POST"])
+def Display_Tables():
+    data = sql.ShowTable()
+    if desc:
+        args = request.args.to_dict().get("Table_number")
+        desc = sql.DesTable(args)
+        return render_template('database.html', tables=data, desc=desc)
+    else: 
+        return render_template('database.html', tables=data)
+
+def ret():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
 	app.run()
